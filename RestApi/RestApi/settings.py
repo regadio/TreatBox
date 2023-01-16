a5a2a8e832e7d6062ca187543ab59779eb827f1e
regadio <rregadion@fpcoruna.afundacion.org>
2023-01-16 13:54:31 +0100

    Creada migracion de base de datos a los modelos de django

--- RestApi/RestApi/settings.py
+++ RestApi/RestApi/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'RestApi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'treatbox',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
