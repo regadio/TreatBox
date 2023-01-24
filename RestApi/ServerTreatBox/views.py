ae59616db166a867f0ba154cf4431a301fff331b
regadio <rregadion@fpcoruna.afundacion.org>
2023-01-24 12:44:25 +0100

    Una vista de ejemplo para ver si funciona creada

--- RestApi/ServerTreatBox/views.py
+++ RestApi/ServerTreatBox/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def ejemplo_view(request):
+    return HttpResponse("Este es un ejemplo de endpoint en Django.")
